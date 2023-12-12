c6f83b98947fffd07915ffadbd00513a624dcbcd
Ankur Jindal <ankur.jindal9@gmail.com>
2023-12-12 20:13:24 +0530

    updated for tat and outside tat list

--- app/controllers/fileMaster.controller.js
+++ app/controllers/fileMaster.controller.js
@@ -37,6 +37,8 @@ function updateTATForFiles(allFiles){
       allFiles[i].dataValues.courierwithintat = 0;
       allFiles[i].dataValues.courieroutsidetat = 0;
       allFiles[i].dataValues.totalCards = allFiles[i].cards.length;
+      allFiles[i].dataValues['courieroutsidetat_list']=[];
+      allFiles[i].dataValues['bureauoutsidetat_list'] =[];
 
       for(let j=0;j<allFiles[i].cards.length;j++){
         allFiles[i].cards[j].dataValues['Bureau_Total_TAT_Days']= Math.floor(( new Date() - new Date(startDateForCardTAT) )/(1000 * 60 * 60 * 24));
@@ -44,6 +46,7 @@ function updateTATForFiles(allFiles){
           allFiles[i].cards[j].dataValues['Courier_Total_TAT_Days']= Math.floor(( new Date() - new Date(allFiles[i].cards[j].dataValues.Bureau_Status_Timestamp) )/(1000 * 60 * 60 * 24));
           allFiles[i].cards[j].dataValues['Courier_TAT_Extra_Days_Passed'] = allFiles[i].cards[j].dataValues['Courier_Total_TAT_Days'];
           allFiles[i].dataValues.courieroutsidetat =  allFiles[i].dataValues.courieroutsidetat + 1;
+          allFiles[i].dataValues['courieroutsidetat_list'].push(allFiles[i].cards[j].dataValues.id)
         }else{
           allFiles[i].cards[j].dataValues['Courier_Total_TAT_Days']=null;
           allFiles[i].cards[j].dataValues['Courier_TAT_Extra_Days_Passed'] =null;
@@ -53,12 +56,35 @@ function updateTATForFiles(allFiles){
 
         if(allFiles[i].cards[j].dataValues['Bureau_TAT_Extra_Days_Passed']>0){
           allFiles[i].dataValues.bureauoutsidetat = allFiles[i].dataValues.bureauoutsidetat + 1;
+          allFiles[i].dataValues['bureauoutsidetat_list'].push(allFiles[i].cards[j].dataValues.id);
         }
 
       }
 
       allFiles[i].dataValues.bureauwithintat  = allFiles[i].dataValues.totalCards  - allFiles[i].dataValues.bureauoutsidetat;
   }
+
+  // create tat and outside tat list
+  for(let i=0;i<allFiles.length;i++){
+    allFiles[i].dataValues['bureauoutsidetat_listData'] =[];
+    allFiles[i].dataValues['bureauwithintat_listData'] =[];
+    allFiles[i].dataValues['courieroutsidetat_listData']=[];
+    allFiles[i].dataValues['courierwithintat_listData']=[];
+
+    for(let j=0;j<allFiles[i].cards.length;j++){
+        if(allFiles[i].dataValues['courieroutsidetat_list'].includes(allFiles[i].cards[j].dataValues.id)){
+          allFiles[i].dataValues['courieroutsidetat_listData'].push(allFiles[i].cards[j].dataValues);
+        }else{
+          allFiles[i].dataValues['courierwithintat_listData'].push(allFiles[i].cards[j].dataValues);
+        }
+        if(allFiles[i].dataValues['bureauoutsidetat_list'].includes(allFiles[i].cards[j].dataValues.id)){
+          allFiles[i].dataValues['bureauoutsidetat_listData'].push(allFiles[i].cards[j].dataValues);
+        }else{
+          allFiles[i].dataValues['bureauwithintat_listData'].push(allFiles[i].cards[j].dataValues);
+        }
+    }
+  }
+
   return allFiles;
 }
 exports.getFileTrackingById = async (req, res) => {
