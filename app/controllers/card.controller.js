0a9fa17b09564433b503c9350dd13864d190e360
ak8431 <amitkumarmaurya8431@gmail.com>
2023-12-21 03:18:28 +0530

    added pull request API details

--- app/controllers/card.controller.js
+++ app/controllers/card.controller.js
@@ -1,16 +1,25 @@
 const db = require("../models");
-const config = require("../config/auth.config");
+// const config = require("../config/auth.config");
 const Card = db.card;
 const AuditLog = db.auditLog;
+const File = db.fileMaster;
 
-const Op = db.Sequelize.Op;
+// const Op = db.Sequelize.Op;
 
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcryptjs");
+// var jwt = require("jsonwebtoken");
+// var bcrypt = require("bcryptjs");
 
 exports.getCardTracking = async (req, res) => {
   try {
-    const cardtracking = await Card.findAll();
+    let findAllConditions = {};
+    if (req.query.fileList) {
+      findAllConditions = {
+        include: [{
+          model: File
+        }]
+      }
+    }
+    const cardtracking = await Card.findAll(findAllConditions);
     res.json(cardtracking);
   } catch (error) {
     res.json({ message: error.message });
